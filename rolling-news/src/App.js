6a9c8f4bceb186f6639d61b86072c204d02d964b
Lucas Gutierrez <lucasismaelgutierrez@gmail.com>
2020-09-12 22:48:22 -0300

    Se agrego una imagen de bienvenida al admin

--- rolling-news/src/App.js
+++ rolling-news/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Bienvenida from "./components/Administrador/Bienvenida";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 
 function App() {
   return (
     <div>
-      
+      <Bienvenida></Bienvenida>
     </div>
   );
 }
