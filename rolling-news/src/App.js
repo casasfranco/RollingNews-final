4f07e637a914cf5371523de4e7b1ae7f2c4331b5
Franco Casas <casassfranco@gmail.com>
2020-09-14 11:59:45 -0300

    Se modifico app.js luego de merge

--- rolling-news/src/App.js
+++ rolling-news/src/App.js
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
-import React from "react";
-import "./App.css";
-import Bienvenida from "./components/Administrador/Bienvenida";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-
-=======
 import React from 'react';
 import './App.css';
 import Usuarios from './components/Administrador/Usuarios';
 import "bootstrap/dist/css/bootstrap.min.css"
->>>>>>> 2c2714327754b8c8b0a4a2ea3acf11b4710fba37
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <Bienvenida></Bienvenida>
-=======
       <Usuarios></Usuarios>
->>>>>>> 2c2714327754b8c8b0a4a2ea3acf11b4710fba37
     </div>
   );
 }
