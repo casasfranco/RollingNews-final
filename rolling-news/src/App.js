7314cbbc2e47a16cdce270b5dea862c62218e29c
Lucas Gutierrez <lucasismaelgutierrez@gmail.com>
2020-09-14 20:59:45 -0300

    Se creo el formulario para agregar una nueva noticia

--- rolling-news/src/App.js
+++ rolling-news/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 import Bienvenida from "./components/Administrador/Bienvenida";
 import Usuarios from './components/Administrador/Usuarios';
-import "bootstrap/dist/css/bootstrap.min.css"
+import NuevaNoticia from "./components/Administrador/NuevaNoticia";
 
 function App() {
   return (
     <div>
       <Bienvenida></Bienvenida>
       <Usuarios></Usuarios>
+      <NuevaNoticia></NuevaNoticia>
     </div>
   );
 }
