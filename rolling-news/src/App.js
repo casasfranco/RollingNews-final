74ae6958b9d04c401f4eff17b4582f3adfd116c7
Lucas Gutierrez <lucasismaelgutierrez@gmail.com>
2020-09-14 21:19:31 -0300

    Se creo la tabla para administrar las noticias del sitio

--- rolling-news/src/App.js
+++ rolling-news/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
 import Bienvenida from "./components/Administrador/Bienvenida";
-import Usuarios from './components/Administrador/Usuarios';
-import "bootstrap/dist/css/bootstrap.min.css"
+import Usuarios from "./components/Administrador/Usuarios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Noticias from "./components/Administrador/Noticias";
 
 function App() {
   return (
     <div>
       <Bienvenida></Bienvenida>
       <Usuarios></Usuarios>
-      
+      <Noticias></Noticias>
     </div>
   );
 }
