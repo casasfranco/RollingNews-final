c153c74c5db4d313e02dbf7e389f559fe743640a
Lucas Gutierrez <lucasismaelgutierrez@gmail.com>
2020-09-15 23:26:29 -0300

    Se hizo prueba de validacion de formulario de nueva noticia

--- rolling-news/src/components/Administrador/NuevaNoticiaForm.js
+++ rolling-news/src/components/Administrador/NuevaNoticiaForm.js
@@ -1,63 +1,103 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
 
 const NuevaNoticiaForm = () => {
+  /*const [validar, setValidar] = useState(true);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("en el publicar")
+
+  }
+  
+  const revisar = () => {
+    if (setValidar() === false){
+    console.log("Por favor complete los campos");
+    }
+      console.log("Todo esta en orden");
+  }*/
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <Fragment>
       <Container>
         <div className="bg-secondary p-5 my-4">
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label className>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group controlId="tituloPrincipalNoticia">
+              <Form.Label>
                 <h5>Titulo</h5>
               </Form.Label>
-              <Form.Control type="text"/>
+              <Form.Control required type="text" placeholder="titulo principal" />
+              <Form.Control.Feedback>bien ahiii</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">
+                Please choose a username.
+              </Form.Control.Feedback>
             </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label className>
+            <Form.Group controlId="copeteNoticia">
+              <Form.Label>
                 <h5>Descripción breve</h5>
               </Form.Label>
-              <Form.Control type="text"/>
+              <Form.Control required type="text"/>
+              <Form.Control.Feedback></Form.Control.Feedback>
             </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label className>
+            <Form.Group controlId="urlImgPrincipalNoticia">
+              <Form.Label>
                 <h5>Imagen principal</h5>
               </Form.Label>
-              <Form.Control type="text"/>
+              <Form.Control required type="text"/>
+              <Form.Control.Feedback></Form.Control.Feedback>
             </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label className>
+            <Form.Group controlId="cuerpoNoticia">
+              <Form.Label>
+                <h5>Cuerpo de la noticia</h5>
+              </Form.Label>
+              <Form.Control required as="textarea" rows="3" />
+              <Form.Control.Feedback></Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="urlImgOpcionalNoticia">
+              <Form.Label>
                 <h5>Imagen opcional</h5>
               </Form.Label>
-              <Form.Control type="text"/>
+              <Form.Control required type="text" />
+              <Form.Control.Feedback></Form.Control.Feedback>
             </Form.Group>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label><h5>Cuerpo de la noticia</h5></Form.Label>
-              <Form.Control as="textarea" rows="3" />
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label className>
+            <Form.Group controlId="categoriaNoticia">
+              <Form.Label>
                 <h5>Categoria</h5>
               </Form.Label>
-              <Form.Control type="text"/>
+              <Form.Control required type="text"/>
+              <Form.Control.Feedback></Form.Control.Feedback>
             </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label className>
+            <Form.Group controlId="autorNoticia">
+              <Form.Label>
                 <h5>Autor</h5>
               </Form.Label>
-              <Form.Control type="text"/>
+              <Form.Control required type="text"/>
+              <Form.Control.Feedback></Form.Control.Feedback>
             </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label className>
+            <Form.Group controlId="publicadaNoticia">
+              <Form.Label>
                 <h5>Fecha de publicación</h5>
               </Form.Label>
-              <Form.Control type="text"/>
+              <Form.Control required type="text"/>
+              <Form.Control.Feedback></Form.Control.Feedback>
             </Form.Group>
-            <Button variant="dark" size="lg" type="submit">
-              Publicar
-            </Button>
+
+            <Button type="submit">Publicar</Button>
           </Form>
         </div>
       </Container>
