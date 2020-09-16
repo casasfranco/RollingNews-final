d85d947d60ed21b1dcfcf85b3116f16c93ca16b3
unknown <gutmariano91@gmail.com>
2020-09-16 10:16:57 -0700

    se termino maquetado de formulario de CategoriaNueva

--- rolling-news/src/App.js
+++ rolling-news/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import './bootstrap.min.css';
 import Header from './components/common/Header';
+import CategoriaNueva from './components/principal/CategoriaNueva';
+
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <CategoriaNueva></CategoriaNueva>
     </div>
   );
 }
