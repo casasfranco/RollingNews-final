2ff86c1d8de1385529ebc20a36591a24e5c12cfa
Lucas Gutierrez <lucasismaelgutierrez@gmail.com>
2020-09-17 20:45:50 -0300

    Se actualizó la rama clienteFront con las validaciones de login y formulario de nueva noticia

--- rolling-news/src/components/Administrador/NuevaNoticiaForm.js
+++ rolling-news/src/components/Administrador/NuevaNoticiaForm.js
@@ -1,103 +1,271 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
+import { useForm } from "react-hook-form";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
 
 const NuevaNoticiaForm = () => {
-  /*const [validar, setValidar] = useState(true);
+  //const [show, setShow] = useState(false);
 
+  //const handleClose = () => setShow(false);
+  //const handleShow = () => setShow(true);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("en el publicar")
+  const { register, errors, handleSubmit } = useForm();
 
-  }
-  
-  const revisar = () => {
-    if (setValidar() === false){
-    console.log("Por favor complete los campos");
-    }
-      console.log("Todo esta en orden");
-  }*/
-
-  const [validated, setValidated] = useState(false);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.stopPropagation();
-    }
-
-    setValidated(true);
+  const onSubmit = (data, e) => {
+    console.log(data);
+    e.target.reset();
   };
 
   return (
     <Fragment>
       <Container>
         <div className="bg-secondary p-5 my-4">
-          <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Group controlId="tituloPrincipalNoticia">
-              <Form.Label>
-                <h5>Titulo</h5>
-              </Form.Label>
-              <Form.Control required type="text" placeholder="titulo principal"/>
-              <Form.Control.Feedback>bien ahiii</Form.Control.Feedback>
-              <Form.Control.Feedback type="invalid">
-                Please choose a username.
-              </Form.Control.Feedback>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Titulo</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Titulo principal"
+                name="tituloPrincipalNoticia"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: "Ingrese un titulo",
+                  },
+                  maxLength: {
+                    value: 30,
+                    message: "No más de 30 carácteres",
+                  },
+                  minLength: {
+                    value: 5,
+                    message: "Mínimo 5 carácteres",
+                  },
+                  pattern: {
+                    value: /^[A-Za-z]+$/i,
+                    message:
+                      "Ingrese un texto válido",
+                  },
+                })}
+              />
+              <span id="errorLogin" className="text-danger mb-2">
+                {errors?.tituloPrincipalNoticia?.message}
+              </span>
             </Form.Group>
             <Form.Group controlId="copeteNoticia">
-              <Form.Label>
-                <h5>Descripción breve</h5>
-              </Form.Label>
-              <Form.Control required type="text"/>
-              <Form.Control.Feedback></Form.Control.Feedback>
+              <Form.Label>Descripción breve</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Información general"
+                name="copeteNoticia"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: "Agregue una descripción",
+                  },
+                  maxLength: {
+                    value: 150,
+                    message: "No más de 150 carácteres",
+                  },
+                  minLength: {
+                    value: 10,
+                    message: "Mínimo 10 carácteres",
+                  },
+                  pattern: {
+                    value: /^[A-Za-z]+$/i,
+                    message:
+                      "Ingrese un texto válido",
+                  },
+                })}
+              />
+              <span id="errorLogin" className="text-danger mb-2">
+                {errors?.copeteNoticia?.message}
+              </span>
             </Form.Group>
             <Form.Group controlId="urlImgPrincipalNoticia">
-              <Form.Label>
-                <h5>Imagen principal</h5>
-              </Form.Label>
-              <Form.Control required type="text"/>
-              <Form.Control.Feedback></Form.Control.Feedback>
+              <Form.Label>Imagen principal</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="https://www.lanacion.com.ar/sociedad/coronavirus-argentina-199-dias-del-primer-contagio-nid2453198"
+                name="urlImgPrincipalNoticia"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: "Agregue una imagen de principal",
+                  },
+                  maxLength: {
+                    value: 100,
+                    message: "No más de 100 carácteres",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Mínimo 5 carácteres",
+                  },
+                  pattern: {
+                    value: /^[A-Za-z]+$/i,
+                    message:
+                      "Ingrese una url",
+                  },
+                })}
+              />
+              <span id="errorLogin" className="text-danger mb-2">
+                {errors?.urlImgPrincipalNoticia?.message}
+              </span>
             </Form.Group>
             <Form.Group controlId="cuerpoNoticia">
-              <Form.Label>
-                <h5>Cuerpo de la noticia</h5>
-              </Form.Label>
-              <Form.Control required as="textarea" rows="3" />
-              <Form.Control.Feedback></Form.Control.Feedback>
+              <Form.Label>Cuerpo de la noticia</Form.Label>
+              <Form.Control as="textarea" rows={10}
+                type="text"
+                placeholder="Desarrollo de la noticia..."
+                name="cuerpoNoticia"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: "Complete el campo requerido",
+                  },
+                  maxLength: {
+                    value: 10000,
+                    message: "No más de 10000 carácteres",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Mínimo 5 carácteres",
+                  },
+                  pattern: {
+                    value: /^[A-Za-z]+$/i,
+                    message:
+                      "Ingrese una url",
+                  },
+                })}
+              />
+              <span id="errorLogin" className="text-danger mb-2">
+                {errors?.cuerpoNoticia?.message}
+              </span>
             </Form.Group>
             <Form.Group controlId="urlImgOpcionalNoticia">
-              <Form.Label>
-                <h5>Imagen opcional</h5>
-              </Form.Label>
-              <Form.Control required type="text" />
-              <Form.Control.Feedback></Form.Control.Feedback>
+              <Form.Label>Imagen secundaria (opcional)</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="https://www.lanacion.com.ar/sociedad/coronavirus-argentina-199-dias-del-primer-contagio-nid2453198"
+                name="urlImgOpcionalNoticia"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: "Agregue una imagen de opcional",
+                  },
+                  maxLength: {
+                    value: 100,
+                    message: "No más de 100 carácteres!",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Mínimo 5 carácteres",
+                  },
+                  pattern: {
+                    value: /^[A-Za-z]+$/i,
+                    message:
+                      "Ingrese una url",
+                  },
+                })}
+              />
+              <span id="errorLogin" className="text-danger mb-2">
+                {errors?.urlImgOpcionalNoticia?.message}
+              </span>
             </Form.Group>
             <Form.Group controlId="categoriaNoticia">
-              <Form.Label>
-                <h5>Categoria</h5>
-              </Form.Label>
-              <Form.Control required type="text"/>
-              <Form.Control.Feedback></Form.Control.Feedback>
+              <Form.Label>Categoria</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Deportes"
+                name="categoriaNoticia"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: "Ingrese una categoria",
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: "No más de 20 carácteres!",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Mínimo 5 carácteres",
+                  },
+                  pattern: {
+                    value: /^[A-Za-z]+$/i,
+                    message:
+                      "Ingrese solo texto",
+                  },
+                })}
+              />
+              <span id="errorLogin" className="text-danger mb-2">
+                {errors?.categoriaNoticia?.message}
+              </span>
             </Form.Group>
             <Form.Group controlId="autorNoticia">
-              <Form.Label>
-                <h5>Autor</h5>
-              </Form.Label>
-              <Form.Control required type="text"/>
-              <Form.Control.Feedback></Form.Control.Feedback>
+              <Form.Label>Autor</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Juan Perez"
+                name="autorNoticia"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: "Ingrese el nombre del autor de la nota",
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: "No más de 20 carácteres!",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Mínimo 5 carácteres",
+                  },
+                  pattern: {
+                    value: /^[A-Za-z]+$/i,
+                    message:
+                      "Ingrese solo texto",
+                  },
+                })}
+              />
+              <span id="errorLogin" className="text-danger mb-2">
+                {errors?.autorNoticia?.message}
+              </span>
             </Form.Group>
             <Form.Group controlId="publicadaNoticia">
-              <Form.Label>
-                <h5>Fecha de publicación</h5>
-              </Form.Label>
-              <Form.Control required type="text"/>
-              <Form.Control.Feedback></Form.Control.Feedback>
+              <Form.Label>Fecha de publicación</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="22/09/2020"
+                name="publicadaNoticia"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: "Ingrese una fecha válida",
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: "No más de 20 carácteres!",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Mínimo 5 carácteres",
+                  },
+                  pattern: {
+                    value: /^[A-Za-z]+$/i,
+                    message:
+                      "Ingrese una fecha válida",
+                  },
+                })}
+              />
+              <span id="errorLogin" className="text-danger mb-2">
+                {errors?.publicadaNoticia?.message}
+              </span>
             </Form.Group>
 
-            <Button type="submit">Publicar</Button>
+            <Button variant="dark" size="lg" type="submit">
+              Publicar 
+            </Button>
           </Form>
         </div>
       </Container>
