d1277d9d7807834f478926eb276407f8caf7839e
Silvia Granara <masil79@gmail.com>
2020-09-17 18:20:25 -0300

    validacion del login

--- rolling-news/src/components/common/Barra.js
+++ rolling-news/src/components/common/Barra.js
@@ -6,6 +6,7 @@ import Image from "react-bootstrap/Image";
 import Dropdown from "react-bootstrap/Dropdown";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import { useForm } from "react-hook-form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
 
@@ -14,6 +15,13 @@ const Barra = () => {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const { register, errors, handleSubmit } = useForm();
+
+  const onSubmit = (data, e) => {
+    console.log(data);
+    e.target.reset();
+  };
 
   return (
     <div>
@@ -27,7 +35,7 @@ const Barra = () => {
 
         <Nav className="text-center" inline>
           <Button className="mx-2 shadow" variant="dark" onClick={handleShow}>
-            Login
+            Ingresar
           </Button>
 
           <Modal show={show} onHide={handleClose}>
@@ -40,15 +48,67 @@ const Barra = () => {
               ></Image>
             </Modal.Header>
             <Modal.Body>
-              <Form>
+              <Form onSubmit={handleSubmit(onSubmit)}>
                 <Form.Group controlId="formBasicEmail">
                   <Form.Label>Usuario</Form.Label>
-                  <Form.Control type="text" placeholder="Nombre de usuario" />
+                  <Form.Control
+                    type="text"
+                    placeholder="Nombre de usuario"
+                    name="nombreUsuario"
+                    ref={register({
+                      required: {
+                        value: true,
+                        message: "Campo obligatorio",
+                      },
+                      maxLength: {
+                        value: 30,
+                        message: "No más de 30 carácteres!",
+                      },
+                      minLength: {
+                        value: 3,
+                        message: "Mínimo 3 carácteres",
+                      },
+                      pattern: {
+                        value: /^[A-Za-z]+$/i,
+                        message:
+                          "Solo puede contener letras mayúsculas o minúsculas",
+                      },
+                    })}
+                  />
+                  <span id="errorLogin" className="text-danger mb-2">
+                    {errors?.nombreUsuario?.message}
+                  </span>
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
                   <Form.Label>Contraseña</Form.Label>
-                  <Form.Control type="password" placeholder="Contraseña" />
+                  <Form.Control
+                    type="password"
+                    placeholder="Contraseña"
+                    name="passUsuario"
+                    ref={register({
+                      required: {
+                        value: true,
+                        message: "campo obligatorio",
+                      },
+                      maxLength: {
+                        value: 30,
+                        message: "No más de 30 carácteres!",
+                      },
+                      minLength: {
+                        value: 6,
+                        message: "Mínimo 6 carácteres",
+                      },
+                      pattern: {
+                        value: /^[A-Za-z]+$/i,
+                        message:
+                          "Solo puede contener letras mayúsculas o minúsculas",
+                      },
+                    })}
+                  />
+                  <span id="errorLogin" className="text-danger mb-2">
+                    {errors?.passUsuario?.message}
+                  </span>
                 </Form.Group>
 
                 <Button variant="dark" size="sm" type="submit">
@@ -85,7 +145,7 @@ const Barra = () => {
             <Nav.Link href="#features">Espectáculos</Nav.Link>
             <Nav.Link href="#pricing">Tecnología</Nav.Link>
             <Nav.Link href="#pricing">Deportes</Nav.Link>
-          </Nav>
+            </Nav>
 
           <Dropdown>
             <Dropdown.Toggle
@@ -94,13 +154,14 @@ const Barra = () => {
               variant="danger"
               id="dropdown-basic"
             >
-              <b>Categorías</b>
+              <b>Secciones</b>
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
               <Dropdown.Item href="#/action-1">Economia</Dropdown.Item>
               <Dropdown.Item href="#/action-2">Internacional</Dropdown.Item>
               <Dropdown.Item href="#/action-3">Policiales</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Fotografía</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
         </Navbar.Collapse>
