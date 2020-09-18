70e8ef7bb8363ec0a6a71023471583437e91af7c
Franco Casas <casassfranco@gmail.com>
2020-09-18 14:19:46 -0300

    Se creo un state para guardar los datos de categoria

--- rolling-news/src/components/Administrador/categoria/CategoriaNueva.js
+++ rolling-news/src/components/Administrador/categoria/CategoriaNueva.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
@@ -7,12 +7,13 @@ import { useForm } from "react-hook-form";
 
 const CategoriaNueva = () => {
   const { register, errors, handleSubmit } = useForm();
+  const [datos, setDatos] = useState("")
 
   const onSubmit = (data, e) => {
     console.log(data);
+    setDatos(data);
     e.target.reset();
   };
-
   return (
     <div>
       <Jumbotron>
