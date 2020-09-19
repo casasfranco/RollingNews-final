2637c4791cc69c4866d545b44d8e17e061ea8479
Alvaro <alvaritovalenzuela13@gmail.com>
2020-09-19 17:05:01 -0300

    Se cambiaron los fragemntos de App.js

--- rolling-news/src/App.js
+++ rolling-news/src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"
-import Bienvenida from "./components/Administrador/Bienvenida";
-import Usuarios from './components/Administrador/Usuarios';
-import NuevaNoticia from "./components/Administrador/NuevaNoticia";
-import Noticias from "./components/Administrador/Noticias";
-import Suscripcion from "./components/Administrador/Suscripcion";
+import Suscripcion from "./components/Administrador/Suscripcion.js"
+import Footer from "./components/common/Footer.js"
 
 
 function App() {
   return (
     <div>
-      {/* <Bienvenida></Bienvenida>
-      <Usuarios></Usuarios>
-      <NuevaNoticia></NuevaNoticia>
-      <Noticias></Noticias> */}
       <Suscripcion />
-
+      <Footer />
       
     </div>
   );
