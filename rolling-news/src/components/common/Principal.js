ae55d4a324eadfb3257569ab715ff71fa82b736b
Silvia Granara <masil79@gmail.com>
2020-09-21 19:19:17 -0300

    se borro card de api y se agrego noticias secundaria

--- rolling-news/src/components/common/Principal.js
+++ rolling-news/src/components/common/Principal.js
@@ -4,34 +4,21 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import NoticiaDestacada from "./NoticiaDestacada.js";
+import NoticiaSecundaria from "./NoticiaSecundaria.js";
 
 const Principal = () => {
   return (
     <div>
-      <Card>
-        <Card.Body>
-          {" "}
-          <Row>
-            <Col>
-              <Card className="p-2" border="dark" id="">
-                <h3>aqui van las monedas</h3>
-              </Card>
-            </Col>
-            <Col>
-              <Card className="p-2" border="dark" id="">
-                <h3>aqui va el clima</h3>
-              </Card>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
+      <div className="row">
+        <h1>Apis</h1>
+      </div>
 
       <div className="container-fluid">
         <div className="row justify-content-between">
           {/* Cards */}
           <div className="col-sm-10 col-md-8 col-xl-9">
             <div className="row">
-              <div className="col-sm-12 col-md-6 col-xl-4">
+              <div className="col-sm-12 col-md-6 col-xl-8">
                 <NoticiaDestacada></NoticiaDestacada>
               </div>
               <div className="col-sm-12 col-md-6 col-xl-4">
@@ -43,10 +30,12 @@ const Principal = () => {
               <div className="col-sm-12 col-md-6 col-xl-4">
                 <NoticiaDestacada></NoticiaDestacada>
               </div>
-              <div className="col-sm-12 col-md-6 col-xl-4">
-                <NoticiaDestacada></NoticiaDestacada>
+              <div className="col-sm-12 col-md-6 col-xl-4 p-0">
+                <NoticiaSecundaria></NoticiaSecundaria>
+                <NoticiaSecundaria></NoticiaSecundaria>
+
               </div>
-              <div className="col-sm-12 col-md-6 col-xl-4">
+              <div className="col-sm-12 col-md-6 col-xl-8">
                 <NoticiaDestacada></NoticiaDestacada>
               </div>
               <div className="col-sm-12 col-md-6 col-xl-4">
@@ -61,9 +50,7 @@ const Principal = () => {
             </div>
             <div className="col-sm-12 col-md-4 col-xl-3 mb-4">
               <div>
-                <Card>
-                  <Card.Body>This is some text within a card body.</Card.Body>
-                </Card>
+               
               </div>
             </div>
           </div>
