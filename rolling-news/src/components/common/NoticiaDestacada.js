c5ab83bffa03b036e2f27d1cb2a9d0a980f1726b
Silvia Granara <masil79@gmail.com>
2020-09-21 15:53:43 -0300

    cambios en noticias destacadas

--- rolling-news/src/components/common/NoticiaDestacada.js
+++ rolling-news/src/components/common/NoticiaDestacada.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React from "react";
+import Card from "react-bootstrap/esm/Card";
+import { Link } from "react-router-dom";
 
 const NoticiaDestacada = () => {
-    return (
-        <div>
-        <Card className=" p-0 mt-0  w-100 h-75">
+  return (
+    <div>
+      <Card className=" p-0 mt-0  w-100 h-75">
         <Card.Img
           className="mt-0 w-100 h-50"
           src="https://picsum.photos/300/150?random"
         ></Card.Img>
         <Card.Body>
           <Card.Title className="m-0">Titulo</Card.Title>
-          
-          <Card.Text> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam magnam dignissimos, iure obcaecati perspiciatis beatae, fuga, quam soluta dolores 
-              sint ipsa quasi nulla doloremque porro? Nihil tenetur quod at minus!</Card.Text>
+
+          <Card.Text>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            magnam dignissimos, iure obcaecati perspiciatis beatae, fuga, quam
+            soluta dolores sint ipsa quasi nulla doloremque porro? Nihil tenetur
+            quod at minus!
+          </Card.Text>
         </Card.Body>
-        <Link href="#" className=" text-dark font-italic mx-2 my-3"> Leer mas...</Link>
-
-      
+        <Link href="#" className=" text-dark font-italic mx-2 my-3">
+          {" "}
+          Leer mas...
+        </Link>
       </Card>
-
-            
-        </div>
-    );
+    </div>
+  );
 };
 
 export default NoticiaDestacada;
