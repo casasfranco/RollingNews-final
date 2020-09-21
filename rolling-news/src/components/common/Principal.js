a7d2070a141d4f2eed31910c34fc873964ee9cff
Alvaro <alvaritovalenzuela13@gmail.com>
2020-09-21 15:48:55 -0300

    Se corrigio las cards de la pagina principal

--- rolling-news/src/components/common/Principal.js
+++ rolling-news/src/components/common/Principal.js
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import NoticiaDestacada from "./NoticiaDestacada.js";
 
 const Principal = () => {
   return (
@@ -31,121 +32,31 @@ const Principal = () => {
           <div className="col-sm-10 col-md-8 col-xl-9">
             <div className="row">
               <div className="col-sm-12 col-md-6 col-xl-4">
-                <Card>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
+                <NoticiaDestacada></NoticiaDestacada>
               </div>
               <div className="col-sm-12 col-md-6 col-xl-4">
-                <Card>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
+                <NoticiaDestacada></NoticiaDestacada>
               </div>
               <div className="col-sm-12 col-md-6 col-xl-4">
-                <Card>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
+                <NoticiaDestacada></NoticiaDestacada>
               </div>
               <div className="col-sm-12 col-md-6 col-xl-4">
-                <Card>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
+                <NoticiaDestacada></NoticiaDestacada>
               </div>
               <div className="col-sm-12 col-md-6 col-xl-4">
-                <Card>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
+                <NoticiaDestacada></NoticiaDestacada>
               </div>
               <div className="col-sm-12 col-md-6 col-xl-4">
-                <Card>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
+                <NoticiaDestacada></NoticiaDestacada>
               </div>
               <div className="col-sm-12 col-md-6 col-xl-4">
-                <Card>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
+                <NoticiaDestacada></NoticiaDestacada>
               </div>
               <div className="col-sm-12 col-md-6 col-xl-4">
-                <Card>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
+                <NoticiaDestacada></NoticiaDestacada>
               </div>
               <div className="col-sm-12 col-md-6 col-xl-4">
-                <Card>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
+                <NoticiaDestacada></NoticiaDestacada>
               </div>
             </div>
             <div className="col-sm-12 col-md-4 col-xl-3 mb-4">
